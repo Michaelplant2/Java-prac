69aa022f581bb6077640775a7b2c5bb6473a88a0
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-10-16 16:27:30 -0400

    Initial commit

---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * hello
+ */
+public class hello {
+
+   public static void main(String[] args) {
+      int x = 123;
+      float y = 3.14f;
+      boolean z = false;
+      char symbol = '@';
+      String name = "Mike";
+
+      System.out.println();
+   }
+}
