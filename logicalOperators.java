7abe31e18993a74faebfba8675a7fe623ae9fd8c
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-10-18 15:44:08 -0400

    logical operators

--- logicalOperators.java
+++ logicalOperators.java
@@ -6,10 +6,37 @@ public class logicalOperators {
 
       Scanner scanner = new Scanner(System.in);
 
+      /* Using && */
+      int temp = 85;
+      if(temp < 45) {
+         System.out.println("It is cold outside.");
+      } else if(temp >= 85 && temp <= 100) {
+         System.out.println("It is very hot outside.");
+      } else {
+         System.out.println("Its perfect outside.");
+      }
+
+
+      /* Using ||
+      System.out.println("You are playing a game! Press Q or q to quit.");
+      String response = scanner.next();
+      if(response.equals("q") || response.equals("Q")) {
+         System.out.println("You quit the game.");
+      } else {
+         System.out.println("You are still playing the game.");
+      } */
 
       
-      scanner.close();
+      /* Using !
+      System.out.println("You are playing a game! Press Q or q to quit.");
+      String response = scanner.next();
+      if(!response.equals("q") && !response.equals("Q")) {
+         System.out.println("You are still playing the game.");
+      } else {
+         System.out.println("You quit the game.");
+      } */
 
+      scanner.close();
    }
    
 }
