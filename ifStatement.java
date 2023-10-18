46de82741aae5b75912f879768a6b07d3d74d370
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-10-18 15:12:18 -0400

    if statements

---
+++ ifStatement.java
@@ -0,0 +1,16 @@
+public class ifStatement {
+
+   public static void main(String[] args) {
+
+      int age = 80;
+
+      if(age >= 75) {
+         System.out.println("Your old!!");
+      } else if(age >= 18) {
+         System.out.println("You are an adult!!");
+      } else {
+         System.out.println("You are not an adult.");
+      }
+   }
+
+}
---
+++ switches.java
@@ -0,0 +1,7 @@
+public class switches {
+   
+   public static void main(String[] args) {
+      
+   }
+
+}
