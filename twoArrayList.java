e3175fdee8eda8cc1d4a72644dab478854b86768
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-10-25 16:02:14 -0400

    2d Array List

--- twoArrayList.java
+++ twoArrayList.java
@@ -1,8 +1,30 @@
+import java.util.*;
+
 public class twoArrayList {
 
    public static void main(String[] args) {
       
-      
+      ArrayList<ArrayList<String>> groceryList = new ArrayList();
+
+      ArrayList<String> bakeryList = new ArrayList();
+      bakeryList.add("Pasta");
+      bakeryList.add("Bread");
+      bakeryList.add("Donuts");
+
+      ArrayList<String> produceList = new ArrayList();
+      produceList.add("Tomatoes");
+      produceList.add("Zucchini");
+      produceList.add("Peppers");
+
+      ArrayList<String> drinkList = new ArrayList();
+      drinkList.add("Soda");
+      drinkList.add("Coffee");
+
+      groceryList.add(bakeryList);
+      groceryList.add(produceList);
+      groceryList.add(drinkList);
+
+      System.out.println(groceryList.get(0).get(2));
 
    }
    
